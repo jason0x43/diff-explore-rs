546dbf5888e48cacce865a1a2d8e6fed11b4c941
Jason Cheatham <jason@jasoncheatham.com>
2022-12-22 14:49:35 -0500

    fix: handle commit messages with quotes
    Use custom format rather than JSON for commit logs

--- src/git.rs
+++ src/git.rs
@@ -16,6 +16,20 @@ pub struct Commit {
     pub subject: String,
 }
 
+impl Commit {
+    pub fn from_line(line: &str) -> Commit {
+        let parts: Vec<&str> = line.splitn(6, '|').collect();
+        Commit {
+            commit: String::from(parts[0]),
+            decoration: String::from(parts[1]),
+            author_name: String::from(parts[2]),
+            author_email: String::from(parts[3]),
+            timestamp: String::from(parts[4]).parse().unwrap(),
+            subject: String::from(parts[5]),
+        }
+    }
+}
+
 #[derive(Debug, Clone, Default)]
 pub struct CommitRange {
     pub start: String,
@@ -91,23 +105,19 @@ pub fn git_log() -> Result<Vec<Commit>> {
         .arg("log")
         .arg("--date=iso8601-strict")
         .arg("--decorate")
-        .arg(
-            "--pretty=format:{\
-			  \"commit\":\"%H\",\
-			  \"decoration\":\"%d\",\
-			  \"author_name\":\"%aN\",\
-			  \"author_email\":\"%aE\",\
-			  \"timestamp\":%at,\
-			  \"subject\":\"%s\"\
-			},",
-        )
+        // commit|decoration|author_name|author_email|timestamp|subject
+        .arg("--pretty=format:%H|%d|%aN|%aE|%at|%s")
         .output()
         .expect("unable to read git log");
     let out_str =
         String::from_utf8(output.stdout).expect("invalid output string");
-    let out_clean = out_str.trim_end_matches("\n").trim_end_matches(",");
-    let out_array = format!("[{}]", out_clean);
-    serde_json::from_str(&out_array)
+
+    let mut commits: Vec<Commit> = vec![];
+    for line in out_str.split("\n") {
+        commits.push(Commit::from_line(line));
+    }
+
+    Ok(commits)
 }
 
 const RENAME_THRESHOLD: u16 = 50;
